1b776e32fd035a282658e6e6117fce8b092f85a1
Daniel Bächli <62600114+dlbbld@users.noreply.github.com>
2026-05-27 22:26:43 +0200

    made listOf visibly immutable

--- src/main/java/com/dlb/chess/common/Nulls.java
+++ src/main/java/com/dlb/chess/common/Nulls.java
@@ -285,9 +285,9 @@ public class Nulls {
     return checkResult(Set.of(items));
   }
 
-  @SuppressWarnings({ "unchecked" })
-  public static <E> List<E> listOf(E... items) {
-    return checkResult(List.of(items));
+  @SafeVarargs
+  public static <E> ImmutableList<E> listOf(E... items) {
+    return checkResult(ImmutableList.copyOf(items));
   }
 
   public static <E extends Enum<E>> EnumSet<E> noneOf(Class<E> enumClass) {
