e42c1cd8113a99b622d8c1216e03b1fba1146654
Daniel Bächli <62600114+dlbbld@users.noreply.github.com>
2026-06-14 23:08:42 +0200

    package-info already defined in production package

--- src/test/java/io/github/dlbbld/ashlarchess/adjudication/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2020-2026 Daniel Baechli
-// SPDX-License-Identifier: GPL-3.0-only
-
-@NonNullByDefault
-package io.github.dlbbld.ashlarchess.adjudication;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
